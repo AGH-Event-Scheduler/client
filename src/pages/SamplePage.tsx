d01b0f4ac204fd585696dd4dc4da8afc9ad62371
Rafał Kamiński <r.kaminski008@gmail.com>
2023-09-10 20:31:18 +0200

    [feat] Reformat the code
    AES-91

--- src/pages/SamplePage.tsx
+++ src/pages/SamplePage.tsx
@@ -1,26 +1,25 @@
 import React from "react";
-import { View, StyleSheet} from "react-native";
+import { View, StyleSheet } from "react-native";
 import { AppButton } from "../components/AppButton";
 
 export const SamplePage = () => {
-
   return (
     <View style={styles.container}>
-      <AppButton 
-        onPress={() => {}} 
-        type='secondary'
+      <AppButton
+        onPress={() => {}}
+        type="secondary"
         title={"Hello World"}
       ></AppButton>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 20
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
   },
-})
+});
