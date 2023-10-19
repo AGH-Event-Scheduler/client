e178aaa43eb2c50aa7d09e46de673b5f2320dfb1
kamil <kamil.blazewicz2810@gmail.com>
2023-10-19 22:12:30 +0200

    AES-67 exported custom stack to files, favourite stack fetching only subscribed for user

--- App.tsx
+++ App.tsx
@@ -1,14 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { ActivityIndicator, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { OrganizationListView } from "./src/pages/organizations/OrganizationListView";
-import { ViewLayoutStructure } from "./src/components/ViewLayoutStructure";
-import { OrganizationDetailsView } from "./src/pages/organization-details/OrganizationDetailsView";
-import { EventDetailsView } from "./src/pages/event-details/EventDetailsView";
-import { SamplePage } from "./src/pages/SamplePage";
-import { LoginPageView } from "./src/pages/authentication/LoginPageView";
-import { UserService } from "./src/services/UserService";
+import React, {useEffect, useState} from "react";
+import {ActivityIndicator, View} from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {UserService} from "./src/services/UserService";
+import {MainStack} from "./src/navigationstacks/MainStack";
+import {AuthenticationStack} from "./src/navigationstacks/AuthenticationStack";
 
 const Stack = createNativeStackNavigator();
 
@@ -43,45 +39,14 @@ export default function App() {
         screenOptions={{ headerShown: false, animation: "fade" }}
         initialRouteName={firstScreen}
       >
-        <Stack.Screen name="Login" component={AuthenticationStack} />
-        <Stack.Screen name="Main" component={MainStack} />
+        <Stack.Screen name="Login">
+          {() => <AuthenticationStack stack={Stack}/>}
+        </Stack.Screen>
+        <Stack.Screen name="Main">
+          {() => <MainStack stack={Stack}/>}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
-const MainStack = () => {
-  return (
-    <ViewLayoutStructure navbarVisible={true}>
-      <Stack.Navigator
-        screenOptions={{ headerShown: true, animation: "fade" }}
-        initialRouteName="Home"
-      >
-        {/* Main pages */}
-        <Stack.Screen name="Home" component={SamplePage} />
-        <Stack.Screen name="Favourite" component={SamplePage} />
-        <Stack.Screen name="Organizations" component={OrganizationListView} />
-        <Stack.Screen name="Calendar" component={SamplePage} />
-        <Stack.Screen name="Feed" component={SamplePage} />
-        <Stack.Screen name="Settings" component={SamplePage} />
-        <Stack.Screen name="Create organization" component={SamplePage} />
-        <Stack.Screen name="Your organizations" component={SamplePage} />
-        <Stack.Screen name="Organization" component={OrganizationDetailsView} />
-        <Stack.Screen name="Event" component={EventDetailsView} />
-      </Stack.Navigator>
-    </ViewLayoutStructure>
-  );
-};
 
-const AuthenticationStack = () => {
-  return (
-    <ViewLayoutStructure navbarVisible={false}>
-      <Stack.Navigator
-        screenOptions={{ headerShown: true, animation: "fade" }}
-        initialRouteName="Log in"
-      >
-        {/* Main pages */}
-        <Stack.Screen name="Log in" component={LoginPageView} />
-      </Stack.Navigator>
-    </ViewLayoutStructure>
-  );
 };
---
+++ src/navigationstacks/MainStack.tsx
@@ -0,0 +1,39 @@
+import {ViewLayoutStructure} from "../components/ViewLayoutStructure";
+import {SamplePage} from "../pages/SamplePage";
+import {OrganizationListView} from "../pages/organizations/OrganizationListView";
+import {OrganizationDetailsView} from "../pages/organization-details/OrganizationDetailsView";
+import {EventDetailsView} from "../pages/event-details/EventDetailsView";
+import React from "react";
+
+
+export const MainStack = ({stack}) => {
+  return (
+    <ViewLayoutStructure navbarVisible={true}>
+      <stack.Navigator
+        screenOptions={{headerShown: true, animation: "fade"}}
+        initialRouteName="Home"
+      >
+        {/* Main pages */}
+        <stack.Screen name="Home" component={SamplePage}/>
+        {/*<stack.Screen name="Favourite" component={SamplePage}/>*/}
+        <stack.Screen name="Favourite">
+          {(props) => (
+            <OrganizationListView
+              {...props}
+              navigation={props.navigation || stack.navigation}
+              onlySubscribed={true}
+            />
+          )}
+        </stack.Screen>
+        <stack.Screen name="Organizations" component={OrganizationListView}/>
+        <stack.Screen name="Calendar" component={SamplePage}/>
+        <stack.Screen name="Feed" component={SamplePage}/>
+        <stack.Screen name="Settings" component={SamplePage}/>
+        <stack.Screen name="Create organization" component={SamplePage}/>
+        <stack.Screen name="Your organizations" component={SamplePage}/>
+        <stack.Screen name="Organization" component={OrganizationDetailsView}/>
+        <stack.Screen name="Event" component={EventDetailsView}/>
+      </stack.Navigator>
+    </ViewLayoutStructure>
+  );
+};
