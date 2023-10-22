c1606c4a8c207877e9e26f83292b4f84b3542d99
fkitka <filip5845@gmail.com>
2023-10-22 19:44:51 +0200

    formatting

--- src/components/AppDropdownSelect.tsx
+++ src/components/AppDropdownSelect.tsx
@@ -1,75 +1,83 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { Feather } from "@expo/vector-icons";
-import { TouchableOpacity, Text, StyleSheet, View, TouchableWithoutFeedback } from "react-native"
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export interface DropdownSelectData {
-  index: any,
-  value: string
+  index: any;
+  value: string;
 }
 
 export interface DropdownSelectProps {
-  data: DropdownSelectData[],
-  currentItem: DropdownSelectData,
+  data: DropdownSelectData[];
+  currentItem: DropdownSelectData;
   onItemSelect: (item: DropdownSelectData) => void;
 }
 
-export const AppDropdownSelect = ({data, currentItem, onItemSelect}: DropdownSelectProps) => {
+export const AppDropdownSelect = ({
+  data,
+  currentItem,
+  onItemSelect,
+}: DropdownSelectProps) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(currentItem)
-  
+  const [selectedItem, setSelectedItem] = useState(currentItem);
+
   const handlePress = () => {
-    setIsExpanded(previousState => !previousState);
-  }
+    setIsExpanded((previousState) => !previousState);
+  };
   const handleExpandedPress = (item: DropdownSelectData) => {
     setSelectedItem(item);
     onItemSelect(item);
     setIsExpanded(false);
-  }
+  };
   return (
     <View>
       <TouchableWithoutFeedback onPress={handlePress}>
         <View style={styles.button}>
           <Text style={styles.text}>{selectedItem.value}</Text>
-          { isExpanded 
-              ? <Feather name={"chevron-up"} size={20} color="black" /> 
-              : <Feather name={"chevron-down"} size={20} color="black" /> }
+          {isExpanded ? (
+            <Feather name={"chevron-up"} size={20} color="black" />
+          ) : (
+            <Feather name={"chevron-down"} size={20} color="black" />
+          )}
         </View>
       </TouchableWithoutFeedback>
-      { isExpanded 
-      ? <View style={styles.expanded}>
-          { data.map((item) => 
-            <TouchableOpacity 
-              key={item.index} 
-              style={styles.buttonExpanded} 
+      {isExpanded ? (
+        <View style={styles.expanded}>
+          {data.map((item) => (
+            <TouchableOpacity
+              key={item.index}
+              style={styles.buttonExpanded}
               onPress={() => handleExpandedPress(item)}
-              >
+            >
               <Text style={styles.textExpanded}>{item.value}</Text>
-              { item.index === selectedItem.index
-                ? <Feather name={"check"} size={14} color="black" /> 
-                : null }
-              </TouchableOpacity>
-            )
-          }
-        </View> 
-        : null
-      }
+              {item.index === selectedItem.index ? (
+                <Feather name={"check"} size={14} color="black" />
+              ) : null}
+            </TouchableOpacity>
+          ))}
+        </View>
+      ) : null}
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   button: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     paddingVertical: 8,
-    paddingHorizontal: 10,    
+    paddingHorizontal: 10,
     borderRadius: 6,
-    backgroundColor: '#D6D6D6',
+    backgroundColor: "#D6D6D6",
     minWidth: 120,
-    
   },
   text: {
     textAlign: "center",
@@ -93,12 +101,12 @@ const styles = StyleSheet.create({
     marginRight: "auto",
   },
   buttonExpanded: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     paddingVertical: 10,
-    paddingHorizontal: 10,    
+    paddingHorizontal: 10,
     minWidth: 120,
-  }
+  },
 });
--- src/localization/languages.ts
+++ src/localization/languages.ts
@@ -1,11 +1,11 @@
 import i18next from "./i18next";
 
 interface Language {
-    index: string,
-    translation: string
+  index: string;
+  translation: string;
 }
 
 export const languages: Language[] = [
-    {index: "pl", translation: i18next.t("languages.pl")},
-    {index: "en", translation: i18next.t("languages.en")}
-]
+  { index: "pl", translation: i18next.t("languages.pl") },
+  { index: "en", translation: i18next.t("languages.en") },
+];
