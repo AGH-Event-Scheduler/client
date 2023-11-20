7fea3e2d213a6ccec4023686a7180e24dd26a18f
Filip Kitka <92650047+fkitka@users.noreply.github.com>
2023-11-20 16:03:36 +0100

    AES-105 Add account section and redesign settings (#27)

---
+++ src/pages/settings/AccountSection.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useTranslation } from "react-i18next";
+import { SettingProps, SettingType } from "./SettingsView";
+import { SettingCard } from "./SettingCard";
+import { fetchUser } from "../../api/authentication-api-utils";
+import { UserView } from "../../api/types";
+
+export const AccountSection = () => {
+  const { t } = useTranslation();
+  const [user, setUser] = useState<UserView>({
+    email: "",
+    name: "",
+    lastname: "",
+  });
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = (await fetchUser()) as UserView;
+      setUser(user);
+    };
+
+    getUser();
+  }, []);
+
+  const accountList: SettingProps[] = [
+    {
+      title: user.email,
+      iconName: "mail",
+      setting: SettingType.Email,
+    },
+    {
+      title: t("settings.account.change-password"),
+      iconName: "lock",
+      setting: SettingType.ChangePassword,
+    },
+  ];
+
+  return (
+    <View style={styles.accountList}>
+      <View style={styles.helloContainer}>
+        <Text style={styles.helloPart1}>{t("settings.account.hello")}</Text>
+        <Text style={styles.helloPart2}>{user.name}!</Text>
+      </View>
+      {accountList.map((item) => (
+        <SettingCard
+          key={item.title}
+          title={item.title}
+          iconName={item.iconName}
+          setting={item.setting}
+        ></SettingCard>
+      ))}
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  accountList: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 30,
+  },
+  helloContainer: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    gap: 4,
+  },
+  helloPart1: {
+    color: "gray",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  helloPart2: {
+    color: "#016531",
+    fontWeight: "bold",
+    fontSize: 30,
+    textTransform: "capitalize",
+  },
+});
